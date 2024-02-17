1d737cd3b9191d50c3ef7d7dd2eef3754cb37393
Shezan <63561954+shezann@users.noreply.github.com>
2024-02-17 19:56:44 +0600

    finish table, fix navbar

--- src/components/EventsTable.tsx
+++ src/components/EventsTable.tsx
@@ -1,19 +1,16 @@
-import { useEffect, useState } from "react";
-import { TEvent, TEndpointResponse } from "../utils/types";
+import "./EventsTable.css";
+import { useEffect, useState, useMemo } from "react";
 import { request, gql } from "graphql-request";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { TEvent, TEndpointResponse } from "../utils/types";
+import { useTable, useSortBy } from "react-table";
+import { Table, Thead, Tbody, Tr, Th, Td, Box } from "@chakra-ui/react";
 import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
 
 const EventsTable = () => {
   const [events, setEvents] = useState<TEvent[]>([]);
 
-  // function to return the name of the event given the event id
-  const getEventName = (id: number) => {
-    const event = events.find((event) => event.id === id);
-    return event ? event.name : "";
-  };
-
   const url = "https://api.hackthenorth.com/v3/graphql";
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -27,40 +24,120 @@ const EventsTable = () => {
     fetchData();
   }, []);
 
+  const handleRowClick = (id: Number) => {
+    console.log("Row clicked", id);
+  };
+
+  const eventTypeDisplay = (eventType: string) => {
+    switch (eventType) {
+      case "workshop":
+        return "Workshop";
+      case "activity":
+        return "Activity";
+      case "tech_talk":
+        return "Tech Talk";
+      default:
+        return "";
+    }
+  };
+
+  const data = useMemo(() => events, [events]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Id",
+        accessor: "id",
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Type",
+        accessor: (row) => eventTypeDisplay(row.event_type),
+      },
+      {
+        Header: "Start Time",
+        accessor: (row) => new Date(row.start_time).toISOString(),
+        Cell: ({ value }) =>
+          new Intl.DateTimeFormat("en", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+          }).format(new Date(value)),
+      },
+      {
+        Header: "End Time",
+        accessor: (row) => new Date(row.end_time).toISOString(),
+        Cell: ({ value }) =>
+          new Intl.DateTimeFormat("en", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+          }).format(new Date(value)),
+      },
+      {
+        Header: "Speakers",
+        accessor: (row) =>
+          row.speakers.map((speaker) => speaker.name).join(", "),
+      },
+      {
+        Header: "Status",
+        accessor: "permission",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
+
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Th>Id</Th>
-          <Th>Name</Th>
-          <Th>Type</Th>
-          <Th>Start Time</Th>
-          <Th>End Time</Th>
-          <Th>Description</Th>
-          <Th>Speakers</Th>
-          <Th>Related Events</Th>
-          <Th>Open To</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {events.map((event) => (
-          <Tr key={event.id}>
-            <Td>{event.id}</Td>
-            <Td>{event.name}</Td>
-            <Td>{event.event_type}</Td>
-            <Td>{new Date(event.start_time).toLocaleString()}</Td>
-            <Td>{new Date(event.end_time).toLocaleString()}</Td>
-            <Td>{event.description}</Td>
-            <Td>{event.speakers.map((speaker) => speaker.name).join(", ")}</Td>
-            {<Td>{event.related_events.join(", ")}</Td>}
-            <Td>
-              {event.related_events.map((id) => getEventName(id)).join(", ")}
-            </Td>
-            <Td>{event.permission}</Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <div className="tableContainer">
+      <Table className="eventsTable" {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <Box as="span" ml="2">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </Box>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
+                ))}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,23 @@ import { Text, Button } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <Text fontSize="2xl">Events</Text>
-      <Button colorScheme="blue">Button</Button>
-    </nav>
+    <>
+      <nav className="navbar">
+        <Text fontSize="3xl" fontWeight="bold">
+          Events
+        </Text>
+        <Button
+          colorScheme="blue"
+          variant="solid"
+          borderRadius="0"
+          bg="gray.800"
+          color="white"
+        >
+          Button
+        </Button>
+      </nav>
+      <div className="navbar-border"></div>
+    </>
   );
 };
 
